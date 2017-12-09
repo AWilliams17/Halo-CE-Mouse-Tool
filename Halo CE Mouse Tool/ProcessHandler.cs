6b4af329a193f42540bd73c9d97b8c48d087cf83
Austin <awilliams17411@gmail.com>
2017-12-09 00:05:15 -0600

    fixed bug in processhandler that caused out of bounds exception when checking if string contained .exe

--- Halo CE Mouse Tool/ProcessHandler.cs
+++ Halo CE Mouse Tool/ProcessHandler.cs
@@ -11,19 +11,16 @@ namespace Halo_CE_Mouse_Tool
     {
         public static bool ProcessIsRunning(string process_name)
         { //If user passes process name with ".exe",
-            if (process_name.Substring(process_name.Length - 4) == ".exe")
+            if (process_name.Contains(".exe"))
             {
                 process_name = process_name.Substring(0, process_name.Length - 4); //Strip it out
             }
             Process[] pname = Process.GetProcessesByName(process_name);
             if (pname.Length != 0)
-            {
                 return true;
-            }
             else
-            {
                 return false;
-            }
         }
     }
 }
+
