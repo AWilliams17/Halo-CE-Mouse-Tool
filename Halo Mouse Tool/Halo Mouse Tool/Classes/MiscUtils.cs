a5dac761e6527fc1ec08779f31b63e8d94c5306c
awilliams17 <awilliams17411@gmail.com>
2018-01-13 00:40:24 -0600

    in preperation for refactoring, added miscutils.cs...

---
+++ Halo Mouse Tool/Halo Mouse Tool/Classes/MiscUtils.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Halo_Mouse_Tool.Classes
+{
+    class MiscUtils
+    {
+    }
+}
