d1d4904f70a12d3a9cd30503d63bc66b0c997c3d
awilliams17 <awilliams17411@gmail.com>
2018-09-19 16:20:01 -0500

    TODO.md added

--- Halo-CE-Mouse-Tool/MainWindow.xaml.cs
+++ Halo-CE-Mouse-Tool/MainWindow.xaml.cs
@@ -18,7 +18,7 @@ namespace Halo_CE_Mouse_Tool
     /// <summary>
     /// Interaction logic for MainWindow.xaml
     /// </summary>
-    public partial class MainWindow : Window
+    public partial class MainWindow
     {
         public MainWindow()
         {
